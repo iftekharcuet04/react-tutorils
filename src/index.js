7cc2549e6606797d1e523d175460c54beb69a744
iftekhar <iftekharcuet04@gmail.com>
2019-02-28 13:04:34 +0600

    Clock app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-ReactDOM.render(
-<h1>hi iftekhar</h1>,
-    document.getElementById('root')
-);
+/*let name = 'Iftekhar1'
+let obj = {
+    fnane:'iftekharul',
+    lname: 'islam'
+
+}*/
+//const element = <h1>hi {name} </h1>
+let clock = function () {
+    return <h1>{ new Date().toLocaleTimeString()}</h1>
+}
+
+setInterval(function () {
+    ReactDOM.render(
+        //<h1>hi {obj.fnane} {obj.lname}</h1>
+        clock(),
+        document.getElementById('root')
+    );
+},1000)
+
